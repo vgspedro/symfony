9a21185f8fa20f16cb4f88811352819445e64437
vgspedro <vgspedro@gmail.com>
2019-04-17 00:00:05 +0100

    set timezone

--- src/Kernel.php
+++ src/Kernel.php
@@ -14,6 +14,12 @@ class Kernel extends BaseKernel
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set($_ENV['TIMEZONE']);
+        parent::__construct($environment, $debug);
+    }
+
     public function getCacheDir()
     {
         return $this->getProjectDir().'/var/cache/'.$this->environment;
