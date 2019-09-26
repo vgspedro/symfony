39e9a7b0c94b9a721d9c87b94f47f11153ec3a5b
vgspedro <vgspedro@gmail.com>
2019-09-26 11:56:06 +0100

    menu as service

--- src/Service/Menu.php
+++ src/Service/Menu.php
@@ -6,23 +6,48 @@ class Menu
 
     public function administration() {
 
-        $e[] = ['path' => '#', 'name'=> 'Painel Controlo', 'icon' => 'fas fa-tachometer-alt', 'target' => '', 'id' => 'dashboard' ];
+        //company links
+        $s[] = ['path' => '#', 'name'=> 'Editar', 'icon' => 'far fa-edit fa-fw', 'target' => '', 'id' => 'company', 'color' => 'w3-blue', 'submenu' => [] ];
+        $s[] = ['path' => '#', 'name'=> 'Texto Reservas', 'icon' => 'far fa-file-alt', 'target' => '', 'id' => 'about-us', 'color' => 'w3-blue', 'submenu' => [] ];
+        $s[] = ['path' => '#', 'name'=> 'RGPD', 'icon' => 'far fa-file-alt', 'target' => '', 'id' => 'rgpd', 'color' => 'w3-blue', 'submenu' => [] ];
+        $s[] = ['path' => '#', 'name'=> 'Termos & Condições', 'icon' => 'fas fa-asterisk', 'target' => '', 'id' => 'terms', 'color' => 'w3-blue', 'submenu' => [] ];
+        $s[] = ['path' => '#', 'name'=> 'Logs Rotinas', 'icon' => 'fas fa-cogs', 'target' => '', 'id' => 'list-cron', 'color' => 'w3-blue', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'A Empresa', 'icon' => 'fa fa-home w3-text-blue', 'target' => '', 'id' => 'company', 'submenu' => $s ];
 
-        $e[] = ['path' => '#', 'name'=> 'Disponibilidades', 'icon' => 'far fa-calendar-check', 'target' => '', 'id' => 'available' ];
+        //categories links
+        $sc[] = ['path' => '#', 'name'=> 'Editar/Listar', 'icon' => 'far fa-edit fa-fw', 'target' => '', 'id' => 'category-list', 'color' => 'w3-purple', 'submenu' => [] ];
+        $sc[] = ['path' => '#', 'name'=> 'Nova', 'icon' => 'far fa-plus-square fa-fw', 'target' => '', 'id' => 'category-new', 'color' => 'w3-purple', 'submenu' => [] ];
+        $sc[] = ['path' => '#', 'name'=> 'Feedback', 'icon' => 'fa-fw far fa-comment-dots', 'target' => '', 'id' => 'feedback-list', 'color' => 'w3-purple', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Categorias', 'icon' => 'fa fa-tags fa-fw w3-text-purple', 'target' => '', 'id' => 'category', 'submenu' => $sc ];
 
-        $e[] = ['path' => '#', 'name'=> 'Reservas', 'icon' => 'far fa-calendar-alt fa-fw', 'target' => '', 'id' => 'booking' ];
-		
-        $e[] = ['path' => '#', 'name'=> 'Texto Fácil', 'icon' => 'fas fa-signal', 'target' => '', 'id' => 'easy-text' ];
+        //extra payments links
+        $sp[] = ['path' => '#', 'name'=> 'Listar', 'icon' => 'far fa-list-alt fa-fw', 'target' => '', 'id' => 'list-extra-payment', 'color' => 'w3-amber', 'submenu' => [] ];
+        $sp[] = ['path' => 'create_extra_payment', 'name'=> 'Novo', 'icon' => 'far fa-plus-square fa-fw', 'target' => '', 'id' => '', 'color' => 'w3-amber', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Pagamento Extra', 'icon' => 'far fa-money-bill-alt fa-fw w3-text-amber', 'target' => '', 'id' => 'payments', 'submenu' => $sp ];
 
-		$e[] = ['path' => '#', 'name'=> 'Aviso', 'icon' => 'fa fa-exclamation-triangle', 'target' => '', 'id' => 'warning' ];
+        //galleries links
+        $sg[] = ['path' => '#', 'name'=> 'Editar/Listar', 'icon' => 'far fa-edit fa-fw', 'target' => '', 'id' => 'gallery-list', 'color' => 'w3-green', 'submenu' => [] ];
+        $sg[] = ['path' => '#', 'name'=> 'Nova', 'icon' => 'far fa-plus-square fa-fw', 'target' => '', 'id' => 'gallery-new', 'color' => 'w3-green', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Galeria', 'icon' => 'far fa-images fa-fw w3-text-green', 'target' => '', 'id' => 'gallery', 'submenu' => $sg ];
         
-        $e[] = ['path' => 'index', 'name'=> 'Ver Site', 'icon' => 'fas fa-external-link-alt fa-fw', 'target' => '_blank', 'id' =>''];
-		
-		$e[] = ['path' => 'logout', 'name'=> 'Sair', 'icon' => 'fas fa-sign-out-alt fa-fw', 'target' => '_self', 'id' => ''];
+        // users links
+        $su[] = ['path' => '#', 'name'=> 'Editar/Listar', 'icon' => 'far fa-edit fa-fw', 'target' => '', 'id' => 'user-list', 'color' => 'w3-pink', 'submenu' => [] ];
+        $su[] = ['path' => '#', 'name'=> 'Novo', 'icon' => 'far fa-plus-square fa-fw', 'target' => '', 'id' => 'user-new', 'color' => 'w3-pink', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Utilizadores', 'icon' => 'fas fa-user-shield fa-fw w3-text-pink', 'target' => '', 'id' => 'user', 'submenu' => $su ];
+        
+        //unique links
+        $e[] = ['path' => '#', 'name'=> 'Painel Controlo', 'icon' => 'fas fa-tachometer-alt', 'target' => '', 'id' => 'dashboard', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Disponibilidades', 'icon' => 'far fa-calendar-check', 'target' => '', 'id' => 'available', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Reservas', 'icon' => 'far fa-calendar-alt fa-fw', 'target' => '', 'id' => 'booking', 'submenu' => [] ];
+        $e[] = ['path' => '#', 'name'=> 'Texto Fácil', 'icon' => 'fas fa-signal', 'target' => '', 'id' => 'easy-text', 'submenu' => [] ];
+		$e[] = ['path' => '#', 'name'=> 'Aviso', 'icon' => 'fa fa-exclamation-triangle', 'target' => '', 'id' => 'warning', 'submenu' => [] ];
+        $e[] = ['path' => 'index', 'name'=> 'Ver Site', 'icon' => 'fas fa-external-link-alt fa-fw', 'target' => '_blank', 'id' =>'', 'submenu' => [] ];
+		$e[] = ['path' => 'logout', 'name'=> 'Sair', 'icon' => 'fas fa-sign-out-alt fa-fw', 'target' => '_self', 'id' => '', 'submenu' => [] ];
         
         return $e;
     }
 
+    //to future develop??
     public function site() {
         $err = [];
         return $err;
