95fc6b55be9b78546d47765e5e28036bf63d4d30
vgspedro <vgspedro@gmail.com>
2020-01-11 13:43:35 +0000

    translations

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -258,7 +258,7 @@ class AdminController extends AbstractController
                         'notes' => $booking->getNotes(),
                         'user_id' => $client->getId(),
                         'deposit' => $moneyFormatter->format($booking->getDepositAmount()),
-                        'payment_status' => strtolower($booking->getPaymentStatus()),
+                        'payment_status' => strtolower('p_'.$booking->getPaymentStatus()),
                         'payment_log' => $booking->getStripePaymentLogs() ? 1 : 0,
                         'payment_status_txt' => strtoupper($translator->trans($booking->getPaymentStatus())),
                         'username' => $client->getUsername(),
